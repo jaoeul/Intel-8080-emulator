28ef80d3e1a03fa894615d292cee1bad602919ca
Joel Engelcrona <engelcronajoel@gmail.com>
2020-09-02 23:01:48 +0200

    Implemented parity function and some opcodes

--- emu.c
+++ emu.c
@@ -55,6 +55,26 @@ unimp(State *state)
     exit(1);
 }
 
+static int
+parity(uint16_t number)
+{
+    int bits_n = 0;
+
+    if ((number & 0x80) != 0) bits_n++;
+    if ((number & 0x40) != 0) bits_n++;
+    if ((number & 0x20) != 0) bits_n++;
+    if ((number & 0x10) != 0) bits_n++;
+    if ((number & 0x8)  != 0) bits_n++;
+    if ((number & 0x4)  != 0) bits_n++;
+    if ((number & 0x2)  != 0) bits_n++;
+    if ((number & 0x1)  != 0) bits_n++;
+
+    if (bits_n % 2 == 0)
+        return 1;
+    else
+        return 0;
+}
+
 static void
 emulate(State *state)
 {
@@ -62,37 +82,79 @@ emulate(State *state)
     // access data / addreses following it easily
     uint8_t *opcode = &state->memory[state->pc];
 
-    // Emulate forever
-    for (;;) {
-        opcode = &state->memory[state->pc];
+    opcode = &state->memory[state->pc];
 
-        switch (*opcode) {
+    switch (*opcode) {
 
-            // NOP
-            case 0x00:
-                state->pc++;
-                break;
+        // Variable to hold answer for arthmetic operations
+        uint16_t answer;
 
-            case 0x01:
-                state->c  = opcode[1];
-                state->b  = opcode[2];
-                state->pc += 3;
-                break;
+        // NOP
+        case 0x00:
+            state->pc++;
+            break;
 
-            default:
-                unimp(state);
-                exit(1);
-        }
+        // Load 16 bit value into register pair
+        // LXI B, D16
+        case 0x01:
+            state->c  = opcode[1];
+            state->b  = opcode[2];
+            state->pc += 3;
+            break;
+
+        // DEC b
+        case 0x05:
+            answer = state->b - 1;
+
+            /// Set the flags
+            // if the answer anded wich 0xff is 0 than the zero flag will be set
+            state->cc.z = ((answer & 0xff) == 0);
+            // if the answer anded with 0x80 (128) is not 0 than the 7 bit is set
+            state->cc.s = ((answer & 0x80) != 0);
+            // if the answer is bigger than the landing size the carry flag will be set
+            state->cc.cy = (answer > 0xff);
+            // if the parity is even set the parity flag
+            state->cc.p = parity(answer);
+
+            // save the result into the accumulator
+            state->a = answer;
+            state->pc++;
+            break;
+
+        // LXI SP, D16
+        case 0x31:
+            state->sp = (opcode[2] << 8) | opcode[1];
+            state->pc += 3;
+            break;
+
+        // JMP addr
+        case 0xc3:
+            // Move third byte 8 bits to the right
+            // bitwise or to add the second byte to the lower space
+            state->pc = (opcode[2]) << 8 | opcode[1];
+            state->pc += 3;
+            break;
+
+        default:
+            unimp(state);
+            exit(2);
     }
 }
 
-int 
+int
 main()
 {
     State *state = calloc(sizeof state, 1);
+
     state->memory_size = read_rom(&state->memory, "./invaders/invaders.full");
 
-    emulate(state);
+    printf("%d\n", parity(15));
+
+    for (;;) {
+        emulate(state);
+    }
+
+    free(state->memory);
 
     return 0;
 }
