f75838267dd19f48e5199071c48723b3c057f10c
Joel Engelcrona <engelcronajoel@gmail.com>
2020-09-06 13:01:36 +0200

    Added CLI driver for dissasembler

--- Makefile
+++ Makefile
@@ -2,3 +2,6 @@ CC=gcc
 
 emu:
 	gcc -g -Wall -Werror dissas.c emu.c -o emu
+
+dis:
+	gcc -g -Wall -Werror dissas.c disdriver.c -o dis
---
+++ disdriver.c
@@ -0,0 +1,15 @@
+#include <stdint.h>
+#include <stdlib.h>
+
+#include "dissas.h"
+
+int
+main(int argc, char **argv)
+{
+    uint8_t *rom;
+    size_t rom_sz = read_rom(&rom, argv[1]);
+    dissas(rom, rom_sz);
+    free(rom);
+    return 0;
+}
+
